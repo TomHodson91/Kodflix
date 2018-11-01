32d8939ec4a76e1276b7167a535f6ae225372cb2
Tom Hodson <tomhodson91@hotmail.com>
2018-11-01 16:53:39 +0000

    Added 6 TV show covers in 2 rows side by side fitted to the browser (#1 #2 #3)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './harryPotterAndTheGobletOfFire.jpg';
+import logo1 from './breakingBad.jpg';
+import logo2 from './doctorWho.png';
+import logo3 from './merlin.jpg';
+import logo4 from './prisonBreak.jpg';
+import logo5 from './westWorld.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
+      <div className="row">
+        <div class="column"> 
+          <img src={logo} alt="logo" />
+        </div>
+        <div class="column">
+          <img src={logo1} alt="logo1" />
+        </div>
+        <div class="column">
+          <img src={logo2} alt="logo2" />
+        </div>
+      </div>
+
+      <div class="row">
+      <div class="column">
+          <img src={logo3} alt="logo3" />
+        </div>
+        <div class="column">
+          <img src={logo4} alt="logo4" />
+        </div>
+        <div class="column">
+          <img src={logo5} alt="logo5" />
+        </div>
+      </div>
+
       </div>
     );
   }
