05ba0d24b109cc03bf80bea6c5cb413b427994af
Tom Hodson <tomhodson91@hotmail.com>
2018-11-07 13:56:33 +0000

    Made a unique detais URL for each TV show link #10

--- src/App.js
+++ src/App.js
@@ -15,15 +15,15 @@ function Gallery() {
   return (
     <div>
       <div className="Row">
-        <Wavey source={logo} name='Harry Potter' />
-        <Wavey source={logo1} name='Breaking Bad' />
-        <Wavey source={logo2} name='Doctor Who' />
+        <Wavey source={logo} name='Harry Potter' url='harry-potter' />
+        <Wavey source={logo1} name='Breaking Bad' url='breaking-bad' />
+        <Wavey source={logo2} name='Doctor Who' url='doctor-who'/>
       </div>
 
       <div className="Row">
-        <Wavey source={logo3} name='Merlin' />
-        <Wavey source={logo4} name='Prison Break' />
-        <Wavey source={logo5} name='Westworld' />
+        <Wavey source={logo3} name='Merlin' url='merlin'/>
+        <Wavey source={logo4} name='Prison Break' url='prison-break'/>
+        <Wavey source={logo5} name='Westworld' url='westworld'/>
       </div>
     </div>
   );
@@ -35,38 +35,31 @@ function Details() {
   return (
     <h2>Hello, this will be the details page for each Movie and TV show :)</h2>
   );
-
 }
-
 
 class Wavey extends React.Component {
   render() {
     return (
-      <Link to="/details">
+      <Link to={this.props.url}>
         <div className="item" >
-
           <img className="column" src={this.props.source} alt="" />
-
           <div className="overlay">
-
             <div className="text">{this.props.name}</div>
           </div>
-
         </div>
       </Link>
     );
   }
 }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={Gallery} />
-        <Route exact path="/details" component={Details} />
+        <Route exact path="/:hiricardo" component={Details} />
       </div>
-
     );
-
   }
 }
 export default App;
