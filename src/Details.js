a848e8157ca66230ef6afffbc41506fff3b35f8a
Tom Hodson <tomhodson91@hotmail.com>
2018-11-27 16:39:55 +0000

    introduced component state #12

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Details() {
+export default class Details extends Component {
+   constructor() {
+    super();
+    this.state = {
+        message: 'Hello, this will be the details page for each Movie and TV show :)'
+    }
+   }
+
+   componentDidMount(){
+    setTimeout(()=> {
+        this.setState({message: 'Coming soon! :)'})
+       },3000)
+   };
+   
+   render(){
     return (
-      <h2>Hello, this will be the details page for each Movie and TV show :)</h2>
+      <h2>{this.state.message}</h2>
     );
+}
 }
