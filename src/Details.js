2d815712469b926ee0f5dc0769fe1f4a15bb71a6
Tom Hodson <tomhodson91@hotmail.com>
2018-11-28 09:54:15 +0000

    added semicolon

--- src/Details.js
+++ src/Details.js
@@ -11,7 +11,7 @@ export default class Details extends Component {
    componentDidMount(){
     setTimeout(()=> {
         this.setState({message: 'Coming soon! :)'})
-       },3000)
+       },3000);
    };
    
    render(){
